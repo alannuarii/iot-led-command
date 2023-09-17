1b467eb59661d3057dc8fcb0557325e946a2ffda
sierrawebid <sierrawebid@gmail.com>
2023-09-18 07:01:12 +0800

    Update

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const http = require('http')
+const http = require("http");
 const mqtt = require("mqtt");
-const socketIo = require('socket.io')
+const socketIo = require("socket.io");
 const cors = require("cors");
 require("dotenv").config();
 
-const port = 3000;
+const port = 3344;
 const app = express();
 
 // Add Middleware
@@ -15,7 +15,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CLIENT,
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
@@ -27,17 +27,39 @@ const mqttClient = mqtt.connect(process.env.MQTT_BROKER);
 // Subscribe ke topik MQTT
 mqttClient.on("connect", () => {
   console.log("Terhubung ke broker MQTT");
-  mqttClient.subscribe("led/alan");
+  mqttClient.subscribe(process.env.SUBSCRIBE);
 });
 
-// Menampilkan pesan MQTT di konsol
+// Subscribe and emit MQTT messages
 mqttClient.on("message", (topic, message) => {
   console.log(`Pesan dari topik ${topic}: ${message.toString()}`);
   io.emit("mqtt-status", message.toString());
 });
 
+// Penanganan pesan dari Client
+io.on("connection", (socket) => {
+  console.log("Client terhubung");
+
+  // Mendengarkan pesan dari Client
+  socket.on("client-action", (action) => {
+    console.log(action);
+
+    // Mengirim perintah ke perangkat IoT berdasarkan aksi dari Client
+    if (action === "LED ON" || action === "LED OFF") {
+      // Kirim pesan MQTT ke perangkat IoT
+      mqttClient.publish("led/alan", action);
+    }
+    // Di sini Anda dapat melakukan tindakan lain sesuai kebutuhan.
+  });
+
+  // Tangani penutupan koneksi Client jika diperlukan
+  socket.on("disconnect", () => {
+    console.log("Client terputus");
+  });
+});
+
 // Menggunakan server Express.js
 server.listen(port, () => {
-  // Command : npm run dev
+  // Command: npm run dev
   console.log(`Example app listening on port http://127.0.0.1:${port}`);
 });
